4b9ee3e8245815df6b05b03c885b0e3d813d2647
Данил Кочура <kochura2017@yandex.ru>
2023-09-26 16:16:16 +0300

    Игнор системных настроек шрифта

--- app/src/main/java/lk/mzpo/ru/MainActivity.kt
+++ app/src/main/java/lk/mzpo/ru/MainActivity.kt
@@ -1,40 +1,34 @@
 package lk.mzpo.ru
 
-import android.annotation.SuppressLint
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.view.WindowCompat
-import lk.mzpo.ru.ui.theme.MzpoTheme
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import lk.mzpo.ru.models.BottomNavigationMenu
-import lk.mzpo.ru.network.retrofit.CourseApi
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import com.google.firebase.messaging.FirebaseMessaging
+import lk.mzpo.ru.network.firebase.FirebaseHelpers
+import lk.mzpo.ru.ui.theme.MzpoTheme
 import kotlin.math.log
+
 
 class MainActivity : ComponentActivity() {
     private lateinit var analytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        adjustFontScale(resources.configuration);
         AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
 //            this.getWindow().setFlags(
@@ -45,16 +39,43 @@ class MainActivity : ComponentActivity() {
 //
 //        }
         analytics = Firebase.analytics
+
+
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
+            val context = LocalContext.current
 
+            FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w("myLog111", "Fetching FCM registration token failed", task.exception)
+                    return@OnCompleteListener
+                }
+                // Get new FCM registration token
+                val token = task.result
+//                Log.d("myLog111", "TOKEN:" + token)
+                // Log and toast
+//            sendToken(token, context)
+                FirebaseHelpers.createOrUpdateToken(token, context)
+                val test = getSharedPreferences("session", Context.MODE_PRIVATE)
+                test.edit().putString("token", token.toString()).apply()
+
+            })
             val nav = rememberNavController()
             MzpoTheme {
-
-
                 NavGraph(navHostController = nav)
             }
+        }
+    }
+    private fun adjustFontScale(configuration: Configuration) {
+        if (configuration.fontScale > 1.30) {
+
+            configuration.fontScale = 1.0f
+            val metrics = resources.displayMetrics
+            val wm = getSystemService(WINDOW_SERVICE) as WindowManager
+            wm.defaultDisplay.getMetrics(metrics)
+            metrics.scaledDensity = configuration.fontScale * metrics.density
+            baseContext.resources.updateConfiguration(configuration, metrics)
         }
     }
 }
