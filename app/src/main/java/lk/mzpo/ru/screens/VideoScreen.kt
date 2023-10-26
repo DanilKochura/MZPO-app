fd5cc42e7e5eaff5f58dd140c4b0e209e05d60db
Данил Кочура <kochura2017@yandex.ru>
2023-10-26 18:35:57 +0300

    Иконка уведомлений и доработки видео

--- app/src/main/java/lk/mzpo/ru/screens/VideoScreen.kt
+++ app/src/main/java/lk/mzpo/ru/screens/VideoScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
@@ -37,6 +38,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.media3.common.MediaMetadata
 import androidx.media3.common.MimeTypes
 import androidx.media3.exoplayer.analytics.AnalyticsListener
@@ -48,20 +50,25 @@ import com.rizzi.bouquet.rememberVerticalPdfReaderState
 import io.sanghun.compose.video.VideoPlayer
 import io.sanghun.compose.video.uri.VideoPlayerMediaItem
 import lk.mzpo.ru.R
+import lk.mzpo.ru.models.study.ActiveMaterials
 import lk.mzpo.ru.ui.components.EmailTextField
 import lk.mzpo.ru.ui.components.PasswordTextField
 import lk.mzpo.ru.ui.theme.Primary_Green
+import lk.mzpo.ru.viewModel.VideoViewModel
+import retrofit2.http.Url
 import java.io.InputStream
+import kotlin.math.log
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun VideoScreen(
-    navHostController: NavHostController, url: String
+    navHostController: NavHostController, video: ActiveMaterials, contract: Int, videoViewModel: VideoViewModel = viewModel()
 ) {
     Box(modifier = Modifier.fillMaxSize()) {
         val state = remember {
-            mutableStateOf(0L)
+            mutableStateOf(0F)
         }
+        val ctx = LocalContext.current
 //        val pdfState = rememberVerticalPdfReaderState(
 //            resource = ResourceType.Remote("https://lk.mzpo-s.ru/build/course_files/1/1_2.pdf"),
 //            isZoomEnable = true,
@@ -77,21 +84,23 @@ fun VideoScreen(
         VideoPlayer(
             mediaItems = listOf(
                 VideoPlayerMediaItem.NetworkMediaItem(
-                    url = "https://lk.mzpo-s.ru/build/videos/$url",
+                    url = "https://lk.mzpo-s.ru/build/videos/${video.activeFile!!.upload}",
                     mediaMetadata = MediaMetadata.Builder().setTitle("Widevine DASH cbcs: Tears").build(),
                     mimeType = MimeTypes.APPLICATION_M3U8,
                 )
             ),
             handleLifecycle = true,
             autoPlay = true,
-            usePlayerController = false,
+            usePlayerController = true,
             enablePip = false,
             handleAudioFocus = true,
 
             volume = 0.5f,  // volume 0.0f to 1.0f
             onCurrentTimeChanged = { // long type, current player time (millisec)
-                Log.e("CurrentTime", it.toString())
-                state.value = it
+                Log.e("CurrentTime", it.div(1000).toFloat().toString())
+                state.value = it/1000F
+
+                videoViewModel.update(ctx, video.id!!, it/1000.toFloat(), video.activeFile!!.size!!.toFloat(), video.activeFile!!.size!!.toFloat(), contract)
                 //TODO: should save time it in there
             },
             playerInstance = {
---
+++ app/src/main/java/lk/mzpo/ru/viewModel/VideoViewModel.kt
@@ -0,0 +1,95 @@
+package lk.mzpo.ru.viewModel
+
+import CoursePreview
+import android.content.Context
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavHostController
+import com.android.volley.AuthFailureError
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import lk.mzpo.ru.models.Course
+import lk.mzpo.ru.models.Document
+import lk.mzpo.ru.models.Group
+import lk.mzpo.ru.models.Module
+import lk.mzpo.ru.models.User
+import lk.mzpo.ru.models.UserData
+import lk.mzpo.ru.network.retrofit.AuthData
+import lk.mzpo.ru.network.retrofit.AuthService
+import lk.mzpo.ru.network.retrofit.AuthStatus
+import org.json.JSONArray
+import org.json.JSONObject
+import java.nio.charset.Charset
+import java.time.Duration
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import kotlin.math.log
+
+
+class VideoViewModel  (
+): ViewModel() {
+
+
+    fun update(
+        ctx: Context,
+        id: Int,
+        time: Float,
+        duration: Float,
+        spendTime: Float,
+        contract: Int
+    ) {
+        val queue = Volley.newRequestQueue(ctx)
+
+        val stringRequest: StringRequest = object : StringRequest(
+            Method.POST,
+            "https://lk.mzpo-s.ru/mobile/user/study/saveTime/$id",
+            Response.Listener { response ->
+                Log.d("MyLog", response)
+                Log.d("MyLog", "https://lk.mzpo-s.ru/mobile/user/study/saveTime/$id")
+            },
+
+            Response.ErrorListener { error ->
+                error.printStackTrace()
+            }) {
+            override fun getBodyContentType(): String {
+                return "application/x-www-form-urlencoded"
+            }
+
+            override fun getHeaders(): Map<String, String> {
+                val headers: MutableMap<String, String> =
+                    HashMap()
+                headers["Content-Type"] = "application/x-www-form-urlencoded"
+                headers["Accept"] = "application/json"
+                val test = ctx.getSharedPreferences("session", Context.MODE_PRIVATE)
+                val token = test.getString("token_lk", "")
+                headers["Authorization"] = "Bearer " + token?.trim('"')
+                return headers
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String> {
+                val params: MutableMap<String, String> =
+                    HashMap()
+                params["time"] = time.toString()
+                params["duration"] = duration.toString()
+                params["spendTime"] = spendTime.toString()
+                params["contract"] = contract.toString()
+                return params
+            }
+        }
+
+        queue.add(stringRequest)
+
+
+    }
+
+}
