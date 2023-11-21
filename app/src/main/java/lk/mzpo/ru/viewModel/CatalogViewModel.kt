35db348be97a3bf60f5b3f15ba1842a5a7ee2f00
Данил Кочура <kochura2017@yandex.ru>
2023-11-21 13:26:31 +0300

    Оптимизация и документирование

--- app/src/main/java/lk/mzpo/ru/viewModel/CatalogViewModel.kt
+++ app/src/main/java/lk/mzpo/ru/viewModel/CatalogViewModel.kt
@@ -19,6 +19,7 @@ import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Text
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -37,6 +38,7 @@ import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonParser
 import kotlinx.coroutines.CoroutineScope
@@ -61,203 +63,150 @@ import kotlin.math.log
 
 
 class CatalogViewModel
-        (
-        ): ViewModel()
-{
-        val courses: MutableState<List<CoursePreview>> = mutableStateOf(emptyList())
-        val courses_selected: MutableState<List<CoursePreview>> = mutableStateOf(emptyList())
+    (
+) : ViewModel() {
 
-        val loaded = mutableStateOf(false)
-        val categories: MutableState<List<Category>> = mutableStateOf(emptyList())
-        val h1 = mutableStateOf("")
-        val selected_cat = mutableStateOf(0)
+    val courses = mutableListOf<CoursePreview>() // общий массив полученных курсов
+    val courses_selected: MutableState<List<CoursePreview>> =
+        mutableStateOf(emptyList()) // выбранные курсы при фильтрации на страницах факультетов
 
-        fun getCourses(context: Context, url: String)
-        {
-                        val courses_ar = arrayListOf<CoursePreview>()
-                        val url1 = "https://lk.mzpo-s.ru/mobile/catalog/$url"
-                        val queue = Volley.newRequestQueue(context)
-                        val sRequest = StringRequest(
-                                Request.Method.GET,
-                                url1,
-                                {
-                                                response ->
-                                        val mainobj = JSONArray(response)
+    val maxPageExpired: MutableState<Boolean> =
+        mutableStateOf(false) // флаг достижения последней страницы
 
-                                        for(i in 0 until  mainobj.length())
-                                        {
-                                                val item = mainobj[i] as JSONObject
-                                                val prices = item.getJSONObject("prices")
-                                                courses_ar.add(CoursePreview(
-                                                        item.getInt("id"),
-                                                        item.getString("image"),
-                                                        item.getString("name"),
-                                                        item.getString("prefix"),
-                                                        item.getInt("hours"),
-                                                        Prices(
-                                                                try {
-                                                                        prices.getInt("sale15")
-                                                                } catch (e: Exception)
-                                                                {
-                                                                        null
-                                                                },
-                                                                try {
-                                                                        prices.getInt("ind")
-                                                                } catch (e: Exception)
-                                                                {
-                                                                        null
-                                                                },
-                                                                try {
-                                                                        prices.getInt("weekend")
-                                                                } catch (e: Exception)
-                                                                {
-                                                                        null
-                                                                },
-                                                                try {
-                                                                        prices.getInt("dist")
-                                                                } catch (e: Exception)
-                                                                {
-                                                                        null
-                                                                },
-                                                        ),
-                                                        item.getInt("category"),
-                                                        item.getString("doctype")
+    val loaded = mutableStateOf(false) // флаг успешной загрузки контента
+    val categories: MutableState<List<Category>> =
+        mutableStateOf(emptyList()) // массив категорий для страниц факультетов
+    val h1 = mutableStateOf("") //заголовок категории
+    val selected_cat = mutableStateOf(0) // выбранная категория для страниц факультетов
+
+    val page = mutableStateOf(0) // номер следующей страницы для подгрузки
 
 
-                                                ))
+    /**
+     * Получение списка курсов для выбранной категории
+     * @param context - Контекст для очереди
+     * @param url - Алиас выбранной категории
+     */
+    fun getCourses(context: Context, url: String) {
+        //region Получение курсов
+        val url1 = "https://lk.mzpo-s.ru/mobile/catalog/$url/${page.value}"
+        val queue = Volley.newRequestQueue(context)
+        val sRequest = StringRequest(
+            Request.Method.GET,
+            url1,
+            { response ->
+                val mainobj = JSONArray(response)
 
-                                        }
-                                        courses.value = courses_ar
-                                        courses_selected.value = courses_ar
-                                        loaded.value = true
+                for (i in 0 until mainobj.length()) {
+                    try {
+                        val item = mainobj[i] as JSONObject
+                        val gson = Gson()
+                        val course = gson.fromJson(item.toString(), CoursePreview::class.java)
+                        courses.add(course)
+                    } catch (_: Exception) {
+                    }
 
-                                },
-                                {
-
-                                        Log.d("MyLog", "VolleyError: $it")
-                                }
-                        )
-                        queue.add(sRequest)
-
-
-
-                        val cats = arrayListOf<Category>()
-                        val url_cats = "https://lk.mzpo-s.ru/mobile/category/$url"
-                        val sRequest1 = StringRequest(
-                                Request.Method.GET,
-                                url_cats,
-                                {
-                                                response ->
-
-                                        try {
-                                                val mainobj = JSONObject(response).getJSONArray("children")
-                                                h1.value = JSONObject(response).getString("name")
-                                                for(i in 0 until  mainobj.length())
-                                                {
-                                                        val item = mainobj[i] as JSONObject
-                                                        cats.add(
-                                                                Category(item.getInt("id"),item.getString("name"), item.getString("alias"), item.getInt("parent_id"), null, item.getString("image"), item.getInt("amount"))
-                                                                                                                )
-
-
-                                                }
-                                        } catch (e: Exception)
-                                        {
-                                                Log.d("MyLog", e.toString())
-                                        }
-
-
-                                        categories.value = cats
-
-
-
-                                },
-                                {
-                                        Log.d("MyLog", "VolleyError: $it")
-                                }
-                        )
-
-                queue.add(sRequest1)
                 }
+                courses_selected.value = courses // по умолчанию выбраны все курсы
+                page.value++
+                loaded.value = true
 
-        fun searchCourses(context: Context, url: String)
-        {
-                        val courses_ar = arrayListOf<CoursePreview>()
-                        val url1 = "https://lk.mzpo-s.ru/mobile/search?q=$url"
-                        Log.d("MyLog", url1)
+            },
+            {
 
-                        val queue = Volley.newRequestQueue(context)
-                        val sRequest = StringRequest(
-                                Request.Method.GET,
-                                url1,
-                                {
-                                                response ->
-                                        Log.d("MyLog", response)
-                                        val mainobj = JSONArray(response)
-                                        for(i in 0 until  mainobj.length())
-                                        {
-                                                val item = mainobj[i] as JSONObject
-                                                val prices = item.getJSONObject("prices")
-                                                courses_ar.add(CoursePreview(
-                                                        item.getInt("id"),
-                                                        item.getString("image"),
-                                                        item.getString("name"),
-                                                        item.getString("prefix"),
-                                                        item.getInt("hours"),
-                                                        Prices(
-                                                                try {
-                                                                        prices.getInt("sale15")
-                                                                } catch (e: Exception)
-                                                                {
-                                                                        null
-                                                                },
-                                                                try {
-                                                                        prices.getInt("ind")
-                                                                } catch (e: Exception)
-                                                                {
-                                                                        null
-                                                                },
-                                                                try {
-                                                                        prices.getInt("weekend")
-                                                                } catch (e: Exception)
-                                                                {
-                                                                        null
-                                                                },
-                                                                try {
-                                                                        prices.getInt("dist")
-                                                                } catch (e: Exception)
-                                                                {
-                                                                        null
-                                                                },
-                                                        ),
-                                                        item.getInt("category"),
-                                                        item.getString("doctype")
+                Log.d("MyLog", "VolleyError: $it")
+            }
+        )
+        queue.add(sRequest)
+        //endregion
 
 
-                                                ))
+        //region Получение вложенных категорий категорий
+        val cats = arrayListOf<Category>()
+        val url_cats = "https://lk.mzpo-s.ru/mobile/category/$url"
+        val sRequest1 = StringRequest(
+            Request.Method.GET,
+            url_cats,
+            { response ->
 
-                                        }
-                                        courses.value = courses_ar
-                                        courses_selected.value = courses_ar
-                                        loaded.value = true
-                                        h1.value = "Вот что мы нашли по запросу \"$url\""
-
-                                },
-                                {
-                                        Log.d("MyLog", "VolleyError: $it")
-                                }
+                try {
+                    val mainobj = JSONObject(response).getJSONArray("children")
+                    h1.value = JSONObject(response).getString("name")
+                    for (i in 0 until mainobj.length()) {
+                        val item = mainobj[i] as JSONObject
+                        cats.add(
+                            Category(
+                                item.getInt("id"),
+                                item.getString("name"),
+                                item.getString("alias"),
+                                item.getInt("parent_id"),
+                                null,
+                                item.getString("image"),
+                                item.getInt("amount")
+                            )
                         )
-                        queue.add(sRequest)
 
+
+                    }
+                } catch (e: Exception) {
+                    Log.d("MyLog", e.toString())
                 }
 
 
+                categories.value = cats
 
 
+            },
+            {
+                Log.d("MyLog", "VolleyError: $it")
+            }
+        )
 
+        queue.add(sRequest1)
+        //endregion
+    }
 
+    /**
+     * Получение курсов по запросу из строки поиска
+     * @param context - Контекст для очереди
+     * @param query - Строка запроса (минимум 3 символа)
+     */
+    fun searchCourses(context: Context, query: String) {
+        val url1 = "https://lk.mzpo-s.ru/mobile/search?q=$query"
 
+        if (maxPageExpired.value) { // не делать повторный запрос, если ЛК вернул ошибку
+            return
         }
+        val queue = Volley.newRequestQueue(context)
+        val sRequest = StringRequest(
+            Request.Method.GET,
+            url1,
+            { response ->
+                Log.d("MyLog", response)
+                val mainobj = JSONArray(response)
+                for (i in 0 until mainobj.length()) {
+                    val item = mainobj[i] as JSONObject
+                    val gson = Gson()
+                    val course = gson.fromJson(item.toString(), CoursePreview::class.java)
+                    courses.add(course)
+                }
+
+                courses_selected.value = courses
+                loaded.value = true
+                h1.value = "Вот что мы нашли по запросу \"$query\""
+
+            },
+            {
+                Log.d("MyLog", "VolleyError: $it")
+                maxPageExpired.value = true
+            }
+        )
+        queue.add(sRequest)
+
+    }
+
+
+}
 
 
 
